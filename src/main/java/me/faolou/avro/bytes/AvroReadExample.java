fc684d50d4ad99e8472ece47579267d8b4d9d5fc
Walter <coolwaterever@gmail.com>
2014-11-08 17:49:46 +0800

    add avro binary example

--- src/main/java/me/faolou/avro/bytes/AvroReadExample.java
+++ src/main/java/me/faolou/avro/bytes/AvroReadExample.java
@@ -1,12 +1,31 @@
 package me.faolou.avro.bytes;
 
+import me.faolou.avro.User;
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+
 /**
  * Apache avro data bytes deserializing example
  * Created by zhangkai on 9/24/14.
  */
 public class AvroReadExample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        FileInputStream fis = FileUtils.openInputStream(new File("sam.avro"));
 
+        DatumReader<User> datumReader = new SpecificDatumReader<User>(User.class);
+        BinaryDecoder binaryDecoder = DecoderFactory.get().binaryDecoder(fis, null);
+        User user = datumReader.read(null, binaryDecoder);
+
+        System.out.println(user);
+
+        IOUtils.closeQuietly(fis);
     }
 }
--- src/main/java/me/faolou/avro/bytes/AvroWriteExample.java
+++ src/main/java/me/faolou/avro/bytes/AvroWriteExample.java
@@ -1,9 +1,16 @@
 package me.faolou.avro.bytes;
 
 import me.faolou.avro.User;
+import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.DatumWriter;
 import org.apache.avro.io.DirectBinaryEncoder;
+import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * Apache avro data bytes serializing example
@@ -11,13 +18,20 @@ import org.apache.avro.specific.SpecificDatumWriter;
  */
 public class AvroWriteExample {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception{
+        FileOutputStream fos = FileUtils.openOutputStream(new File("james.avro"));
         User user = User.newBuilder()
                 .setName("James")
                 .setFavoriteNumber(10)
                 .setFavoriteColor("Black")
                 .build();
 
-        DatumWriter<User> datumWriter = new SpecificDatumWriter<User>();
+        DatumWriter<User> datumWriter = new SpecificDatumWriter<User>(User.class);
+        BinaryEncoder binaryEncoder = EncoderFactory.get().binaryEncoder(fos, null);
+        datumWriter.write(user,binaryEncoder);
+        binaryEncoder.flush();
+
+        IOUtils.closeQuietly(fos);
     }
 }
